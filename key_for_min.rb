cc8b2fe3fe3ef0c6c647c3f1bf126d13f8b9eb57
Jae Yong Kim <kim900180@gmail.com>
2018-07-30 20:35:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   smallest_value = nil
   smallest_key = nil
-    if name_hash == {}
+  if name_hash == {}
     return nil
   elsif name_hash != {}
     name_hash.each do |hash_key, hash_value|
